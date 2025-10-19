57d9a2008a828fce286c0eb8dcd0516704c33bf7
puzzle <happypuzzle@126.com>
2025-10-19 20:56:16 +0800

    修改分片大小

--- src/config.js
+++ src/config.js
@@ -12,5 +12,5 @@ export const BASE_URL = import.meta.env.VITE_BASE_URL || 'https://dev.emos.lol'
 export const LOGIN_URL = '/api/link?uuid=9dbf2c0c-7fa2-49a6-a37b-a4ebc9406049&name=上传服务'
 
 // 分片配置
-export const CHUNK_SIZE = 2 * 1024 * 1024 // 2MB 每片
-export const MIN_CHUNK_SIZE = 20 * 1024 * 1024 // 20MB 最小分片大小
+export const CHUNK_SIZE = 100 * 1024 * 1024 // 100MB 每片
+export const MIN_CHUNK_SIZE = 50 * 1024 * 1024 // 50MB 最小分片大小
